d6f7ad3b80db95cfd85ca4b9f3c083a94d64ee27
Anna Wrang <anna.wrang@gmail.com>
2021-03-18 11:06:08 +0100

    initial commit

---
+++ src/main/java/com/annawrang/challenge/ChallengeSvcApplication.java
@@ -0,0 +1,13 @@
+package com.annawrang.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeSvcApplication.class, args);
+	}
+
+}
